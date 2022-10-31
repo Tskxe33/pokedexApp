e2e303db74c7d310fc2c42539301b94eaa91fb60
Petro <petroarva33@gmail.com>
2022-10-31 13:20:53 +0100

    implement ui for welcome screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,8 +1,7 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React, {useEffect} from 'react';
 import {SplashScreenProps} from '../navigators/InitialNavigator';
 import Lottie from 'lottie-react-native';
-import {Fonts} from '../constants/Fonts';
 import LinearGradient from 'react-native-linear-gradient';
 import {Colors} from '../constants/Colors';
 import {AppRoute} from '../constants/AppRoute';
@@ -18,7 +17,6 @@ const SplashScreen: React.FC<SplashScreenProps> = ({navigation}) => {
     <LinearGradient
       style={styles.linearGradient}
       colors={[Colors.LG_DARK, Colors.LG_LIGHT]}>
-      <Text style={styles.splashTitle}>Pokedex</Text>
       <View style={styles.lottieContainer}>
         <Lottie
           source={require('../assets/lotties/splashLottie.json')}
@@ -37,14 +35,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-
-  splashTitle: {
-    fontFamily: Fonts.ROBOTO_REGULAR,
-    fontSize: 48,
-    color: Colors.COLOR_GREY_LIGHT,
-    textTransform: 'uppercase',
-    letterSpacing: 2,
   },
 
   lottieContainer: {
--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,15 +1,42 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {WelcomeScreenProps} from '../navigators/InitialNavigator';
+import {Fonts} from '../constants/Fonts';
+import {Colors} from '../constants/Colors';
+import LinearGradient from 'react-native-linear-gradient';
 
 const WelcomeScreen: React.FC<WelcomeScreenProps> = ({navigation}) => {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
-    </View>
+    <LinearGradient
+      style={styles.welcomeContainer}
+      colors={[Colors.LG_DARK, Colors.LG_LIGHT]}>
+      <Text style={styles.mainHeading}>Pokedex</Text>
+      <Text style={styles.subHeading}>All pokemon in one place</Text>
+    </LinearGradient>
   );
 };
 
 export default WelcomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  welcomeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.LG_DARK,
+  },
+
+  mainHeading: {
+    fontFamily: Fonts.ROBOTO_BOLD,
+    fontSize: 48,
+    color: Colors.COLOR_GREY_LIGHT,
+    textTransform: 'uppercase',
+    letterSpacing: 2,
+  },
+
+  subHeading: {
+    fontFamily: Fonts.ROBOTO_LIGHT,
+    fontSize: 18,
+    color: Colors.COLOR_GREY_LIGHT,
+  },
+});
