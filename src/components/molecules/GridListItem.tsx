c361c866ca272eacbcc0dbcedd613c717c03d9f9
Petro <petroarva33@gmail.com>
2022-11-01 20:41:13 +0100

    create grid screen

---
+++ src/components/molecules/GridListItem.tsx
@@ -0,0 +1,61 @@
+import {Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {Pokemon} from 'pokenode-ts';
+import {Colors, pokemonTypeBackgrounds} from '../../constants/Colors';
+import {Fonts} from '../../constants/Fonts';
+import {useNavigation} from '@react-navigation/native';
+import {AppRoute} from '../../constants/AppRoute';
+
+interface GridListItemProps {
+  item: Pokemon;
+}
+
+const GridListItem: React.FC<GridListItemProps> = ({item}) => {
+  const navigation = useNavigation<any>();
+
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate(AppRoute.POKEMON_DETAILS_SCREEN, {
+          pokemonDetails: item,
+        })
+      }
+      style={{
+        ...styles.itemContainer,
+        backgroundColor: pokemonTypeBackgrounds[item.types[0].type.name],
+      }}>
+      <Text style={styles.itemName}>{item.name}</Text>
+      <Image
+        source={{uri: item.sprites.other?.home.front_default}}
+        style={styles.image}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default GridListItem;
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+
+  image: {
+    width: 60,
+    height: 60,
+    marginBottom: 33,
+    marginTop: 10,
+  },
+
+  itemName: {
+    fontFamily: Fonts.ROBOTO_REGULAR,
+    fontSize: 18,
+    color: Colors.COLOR_WHITE,
+    textTransform: 'capitalize',
+  },
+});
---
+++ src/components/organism/GridList.tsx
@@ -0,0 +1,61 @@
+import {FlatList, ListRenderItem, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Pokemon} from 'pokenode-ts';
+import GridListItem from '../molecules/GridListItem';
+import {Fonts} from '../../constants/Fonts';
+import {Colors} from '../../constants/Colors';
+import {useDispatch} from 'react-redux';
+import {updatePokemons} from '../../redux/actions/PokemonActions';
+
+interface GridListProps {
+  pokemons: Pokemon[];
+}
+
+const GridList: React.FC<GridListProps> = ({pokemons}) => {
+  const dispatch = useDispatch<any>();
+
+  const handleRenderListItem: ListRenderItem<Pokemon> = ({
+    item,
+  }: {
+    item: Pokemon;
+  }) => {
+    return <GridListItem item={item} />;
+  };
+
+  const handleLoadMore = () => {
+    dispatch(updatePokemons());
+  };
+
+  return (
+    <View>
+      <FlatList
+        ListHeaderComponent={
+          <Text style={styles.gridHeading}>Pokemon List</Text>
+        }
+        data={pokemons}
+        renderItem={handleRenderListItem}
+        contentContainerStyle={styles.listContainer}
+        showsVerticalScrollIndicator={false}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={1}
+      />
+    </View>
+  );
+};
+
+export default GridList;
+
+const styles = StyleSheet.create({
+  gridHeading: {
+    textAlign: 'center',
+    marginBottom: 20,
+    fontSize: 20,
+    fontFamily: Fonts.ROBOTO_REGULAR,
+    color: Colors.COLOR_BLACK,
+  },
+  listContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+});
